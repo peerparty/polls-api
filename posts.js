282f2edc1fc42902d2912842b19d34482cb2673a
JBG <jbg@peerparty.org>
2020-03-25 08:22:42 +0000

    fix for consensus flag on comments

--- posts.js
+++ posts.js
@@ -38,6 +38,7 @@ exports.getComment = async (commentId) => {
     if(comment.votes && !vote.changed) comment.votes.push(vote)
     else if(!vote.changed) comment.votes = [ vote ]
   }
+  comment.consensus = getConsensus(comment)
 
   const commentComments = await contract.methods.getCommentComments(commentId)
     .call()
@@ -53,11 +54,20 @@ exports.getComment = async (commentId) => {
 }
 
 function getConsensus(obj) {
+  console.log(">>>>>>", obj)
   if(obj.votes) {
-    const up = obj.votes.reduce((c, v) => (v.up ? c + 1 : c), 0)
-    const down = obj.votes.reduce((c, v) => (!v.up ? c + 1 : c), 0)
-    return (up > 1 && down === 0) || (down > 1 && up ===0)
-  } else return false
+    const votes = obj.votes.filter(v => !v.changed)
+    console.log(votes)
+    const up = votes.reduce((c, v) => (v.up ? c + 1 : c), 0)
+    const down = votes.reduce((c, v) => (!v.up ? c + 1 : c), 0)
+    console.log("UP/DOWN", up, down,  (up > 1 && down === 0) || (down > 1 && up === 0))
+    const c = (up > 1 && down === 0) || (down > 1 && up === 0)
+    console.log("CONSENSUS", obj.comment ? obj.comment : "POST", c)
+    return c
+  } else {
+    console.log("No consensus")
+    return false
+  }
 }
 
 function aggregateConsensus(obj, a) {
@@ -68,7 +78,6 @@ function aggregateConsensus(obj, a) {
 
 exports.getPost = async (postId) => {
   let post = await contract.methods.posts(postId).call()
-  post.consensus = getConsensus(post)
   const postVotes = await contract.methods.getPostVotes(postId).call()
   for(let j = 0; j < postVotes.length; j++) {
     const voteIndex = postVotes[j]
@@ -77,10 +86,11 @@ exports.getPost = async (postId) => {
     else if(!vote.changed) post.votes = [ vote ]
   }
 
+  post.consensus = getConsensus(post)
+
   const postComments = await contract.methods.getPostComments(postId).call()
   for(let j = 0; j < postComments.length; j++) {
     const comment = await this.getComment(postComments[j])
-    comment.consensus = getConsensus(comment)
     if(post.comments) post.comments.push(comment)
     else post.comments = [ comment ]
   }
