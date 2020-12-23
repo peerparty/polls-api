e343cdc42050ebb334c1de4c879cfcdb3148e0ac
JBG <jbg@peerparty.org>
2020-12-23 13:47:32 +0000

    tweaks for new solc

--- posts.js
+++ posts.js
@@ -5,7 +5,7 @@ const Web3 = require('web3'),
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 const source = fs.readFileSync("../consensus.json")
 const contracts = JSON.parse(source)["contracts"]
-const abi = JSON.parse(contracts["consensus.sol:Posts"].abi)
+const abi =contracts["consensus.sol:Posts"].abi
 const contract = new web3.eth.Contract(abi, contractAddr.contractAddress)
 
 exports.getBalance = async addr => {
